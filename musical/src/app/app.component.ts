feccfc3abc41bd4ae64e4aed5e2e4f0835e50690
Marc4919 <marcgeek4919@gmail.com>
2022-05-09 14:35:35 +0200

    Inclusión del dark mode

--- musical/src/app/app.component.ts
+++ musical/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { Platform } from '@ionic/angular';
 
 
 @Component({
@@ -8,8 +9,25 @@ import { MenuController } from '@ionic/angular';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  darkMode = true;
+  constructor(private menu: MenuController, public platform: Platform) {this.initializeApp();}
 
-  constructor(private menu: MenuController) {}
+  initializeApp() {
+    this.platform.ready().then(() => {
+    this.changeDarkMode();
+     });
+    }
+    changeDarkMode(){
+      const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+      if (prefersDark.matches){
+      document.body.classList.toggle('dark');
+        }
+       }
+
+       change() {
+        this.darkMode = !this.darkMode;
+        document.body.classList.toggle('dark');
+         }
   openFirst() {
     this.menu.enable(true, 'first');
     this.menu.open('first');
